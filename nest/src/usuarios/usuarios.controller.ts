734aaf22f44a23ba6f0ff26920ff24ddd893aae6
Malena Rodriguez <maleeroodriguez@gmail.com>
2026-06-03 08:30:59 -0300

    Modificación usuario.controller

--- nest/src/usuarios/usuarios.controller.ts
+++ nest/src/usuarios/usuarios.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsuariosService } from './usuarios.service';
+
 
 @Controller('usuarios')
-export class UsuariosController {}
+export class UsuariosController {
+    constructor(private usuarioService: UsuariosService){}
+
+    @Get('usuarios')
+    obtenerUsuarios() {
+        return this.usuarioService.obtenerUsuarios();
+    }
+}
--- nest/src/usuarios/usuarios.service.ts
+++ nest/src/usuarios/usuarios.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Usuario } from './schema/usuario.schema';
+
+
 
 @Injectable()
-export class UsuariosService {}
+export class UsuariosService {
+
+    constructor(@InjectModel(Usuario.name)
+                private usuarioModel: Model<Usuario>){}
+
+    async obtenerUsuarios(){
+        return await this.usuarioModel.find().select('-password');
+    }
+}
