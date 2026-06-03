b06b42a256275d6f9d1e7e58ac29f22ba7a3cf21
Malena Rodriguez <maleeroodriguez@gmail.com>
2026-06-03 08:13:32 -0300

    Modificación .env

--- nest/src/cloudinary/cloudinary.config.ts
+++ nest/src/cloudinary/cloudinary.config.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: 'dtx7gzchf',
-  api_key: '986128659582661',
-  api_secret: 'Me_UT_E4zclp6bqyIQBGU7vsSGU',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
