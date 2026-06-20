7b707a080ebf77e044fbccd2364d04a230dd0341
Malena Rodriguez <maleeroodriguez@gmail.com>
2026-06-20 12:01:59 -0300

    Estilos de los posts en mi-perfil

--- angular-app/src/app/components/mi-perfil/mi-perfil.ts
+++ angular-app/src/app/components/mi-perfil/mi-perfil.ts
@@ -2,6 +2,9 @@ import { DatePipe } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
 import { Publicacion } from '../../interfaces/publicacion';
 import { PublicacionService } from '../../services/publicacion-service';
+import Swal from 'sweetalert2';
+
+
 
 @Component({
   selector: 'app-mi-perfil',
@@ -12,6 +15,9 @@ import { PublicacionService } from '../../services/publicacion-service';
 export class MiPerfil implements OnInit {
 
   misPublicaciones = signal<Publicacion[]>([]);
+  postSeleccionado : Publicacion | null = null;
+  
+  postAbierto = false;
 
   usuario = JSON.parse(sessionStorage.getItem('usuario')!);
 
@@ -32,6 +38,45 @@ export class MiPerfil implements OnInit {
       },
      (err) => {
       console.log('ERROR DEL GET:', err);
+    })
+    }
+
+  abrirPost(publicacion: Publicacion) {
+  this.postSeleccionado = publicacion;
+  this.postAbierto = true;
+  }
+
+  cerrarPost() {
+    this.postAbierto = false;
+    this.postSeleccionado = null;
+  }
+
+  eliminarPublicacion(publicacion: Publicacion){
+      Swal.fire({
+        title: '¿Estás seguro?',
+        text: 'Estas por borrar esta publicación',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#d8c3a2',
+        cancelButtonColor: '#5f4f39'
+      }).then((result) => {
+  
+        if (result.isConfirmed) {
+  
+          this.publicacionService.eliminar(publicacion._id, this.usuario._id).subscribe(() => {
+             
+            this.misPublicaciones.update(lista => lista.filter(publi => publi._id !== publicacion._id)); // Actualizo y creo nuevo array filtrando las que no tienen el id de la eliminada 
+  
+              Swal.fire(
+                'Eliminada',
+                'La publicación fue eliminada del perfil',
+                'success'
+              );
+            });
+          this.cerrarPost()
+        }
     })
     }
 
