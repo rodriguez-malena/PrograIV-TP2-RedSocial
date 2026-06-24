9ad6bc76a74935621d19856273de2844af80cae7
Malena Rodriguez <maleeroodriguez@gmail.com>
2026-06-24 18:11:26 -0300

    correccion de comentarioModule

--- nest/src/comentarios/comentarios.module.ts
+++ nest/src/comentarios/comentarios.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ComentariosController } from './comentarios.controller';
 import { ComentariosService } from './comentarios.service';
+import { Comentario, ComentarioSchema } from './schema/comentario.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports:[MongooseModule.forFeature([{ name:Comentario.name , schema: ComentarioSchema }])],
   controllers: [ComentariosController],
   providers: [ComentariosService]
 })
