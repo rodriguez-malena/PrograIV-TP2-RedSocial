9c48c98a4dfd973f1524fcb045fc807c0a872030
Malena Rodriguez <maleeroodriguez@gmail.com>
2026-06-24 15:07:45 -0300

    Arreglo de authModule

--- nest/src/auth/auth.module.ts
+++ nest/src/auth/auth.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsuariosModule } from '../usuarios/usuarios.module';
 import { JwtModule } from '@nestjs/jwt';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { Usuario, UsuarioSchema } from '../usuarios/schema/usuario.schema';
 
 @Module({
   imports:[
     JwtModule.register({
     secret: process.env.JWT_SECRET,
     signOptions: { expiresIn: '15m'}
-  }), 
-  UsuariosModule
+  }),
+
+    MongooseModule.forFeature([
+      {
+        name: Usuario.name,
+        schema: UsuarioSchema,
+      },
+    ]), 
   ],
+
   controllers: [AuthController],
   providers: [AuthService]
 })
