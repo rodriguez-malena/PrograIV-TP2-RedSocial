9f0b3e210975b48d056e11ca4b8686697747b80b
Malena Rodriguez <maleeroodriguez@gmail.com>
2026-06-26 17:27:32 -0300

    Habilitar comentarios

--- nest/src/comentarios/comentarios.module.ts
+++ nest/src/comentarios/comentarios.module.ts
@@ -3,11 +3,11 @@ import { ComentariosController } from './comentarios.controller';
 import { ComentariosService } from './comentarios.service';
 import { Comentario, ComentarioSchema } from './schema/comentario.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthService } from '../auth/auth.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports:[MongooseModule.forFeature([{ name:Comentario.name , schema: ComentarioSchema }]),
-          AuthService],
+          AuthModule],
   controllers: [ComentariosController],
   providers: [ComentariosService]
 })
