5e051e2a8792b763a263a9432cee5c5aeaff0002
Malena Rodriguez <maleeroodriguez@gmail.com>
2026-07-01 09:56:49 -0300

    Implementación de gráficos

--- angular-app/src/app/components/dashboard-estadisticas/dashboard-estadisticas.ts
+++ angular-app/src/app/components/dashboard-estadisticas/dashboard-estadisticas.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EstadisticasService } from '../../services/estadisticas-service';
 import { rangoFechasValidator } from '../../validators/rangoFechasValidator';
+import { Chart } from 'chart.js';
 
 
 @Component({
@@ -13,6 +14,9 @@ import { rangoFechasValidator } from '../../validators/rangoFechasValidator';
 export class DashboardEstadisticas implements OnInit {
 
   formularioEstadisticas! : FormGroup
+  chartPosts: any;
+  chartComentarios: any;
+  chartComentariosPost: any;
 
   constructor(private estadisticasService: EstadisticasService,
               private fb: FormBuilder){}
@@ -27,14 +31,81 @@ export class DashboardEstadisticas implements OnInit {
   }
 
   buscar(){
+    this.formularioEstadisticas.markAllAsTouched();
     const { desde, hasta } = this.formularioEstadisticas.value;
 
     if(!desde || !hasta) return;
 
-    this.estadisticasService.postPorUsuario(desde, hasta).subscribe(res => console.log(res));
+    this.obtenerPostPorUsuario(desde, hasta);
+    this.obtenerComentarios(desde, hasta);
+    this.obtenerComentariosPorPost(desde, hasta);
 
-    this.estadisticasService.comentariosPorTiempo(desde, hasta).subscribe(res => console.log(res));
-
-    this.estadisticasService.comentariosPorPost(desde, hasta).subscribe(res => console.log(res));
   }
+
+  // Publicaciones
+    obtenerPostPorUsuario(desde: string, hasta: string){
+      this.estadisticasService.postPorUsuario(desde, hasta).subscribe(res => {
+
+        console.log(res)
+
+        this.chartPosts?.destroy();
+
+        this.chartPosts = new Chart('chartPosts', {
+          type: 'bar',
+          data: {
+            labels: res.map((r: any) => r.usuario),
+            datasets: [{
+              label: 'Publicaciones',
+              data: res.map((r: any) => r.publicaciones)
+            }]
+          }
+        });
+        
+      });
+
+    }
+
+  // Comentarios totales
+    obtenerComentarios(desde: string, hasta: string){
+
+      this.estadisticasService.comentariosPorTiempo(desde, hasta).subscribe((res: any) => {
+        
+        console.log(res)
+        this.chartComentarios?.destroy();
+        
+        this.chartComentarios = new Chart('chartComentarios', {
+          type: 'line',
+          data: {
+            labels: ['Total'],
+            datasets: [{
+              label: 'Comentarios',
+              data: [res.total]
+            }]
+          }
+        })
+    });
+    }
+
+  // Cometarios por publicacion
+    obtenerComentariosPorPost(desde: string, hasta: string){
+
+      this.estadisticasService.comentariosPorPost(desde, hasta).subscribe(res => {
+        
+        console.log(res);
+        this.chartComentariosPost?.destroy();
+
+        this.chartComentariosPost = new Chart('chartComentariosPost', {
+          type: 'pie',
+          data: {
+            labels: res.map((r: any) => r.titulo),
+            datasets: [{
+              data: res.map((r: any) => r.comentarios)
+            }]
+          }
+        });
+      
+      });
+    }
+
+  
 }
